4212e87b5958866539f1a8f3d515cc18a975e054
Chris Rice <cd.rice23@gmail.com>
2023-03-09 18:03:26 -0800

    Added files from other spotify project to jumpstart. Included spotify auth files. Auth functional.

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,42 @@
-import '@/styles/globals.css'
+import React from "react";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { theme } from "../styles/theme";
+import { StyledEngineProvider } from "@mui/material";
+// tested from MUI/next.js tutorial
+import PropTypes from "prop-types";
+import Head from "next/head";
+import { CacheProvider } from "@emotion/react";
+import createEmotionCache from "../lib/createEmotionCache";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// tested from MUI/next.js tutorial
+const clientSideEmotionCache = createEmotionCache();
+
+function MyApp({
+  Component,
+  pageProps,
+  emotionCache = clientSideEmotionCache,
+}) {
+  return (
+    <StyledEngineProvider injectFirst>
+      <CacheProvider value={emotionCache}>
+        {/* cacheprovider and head tested from MUI/next.js tutorial */}
+        <Head>
+          <meta name="viewport" content="initial-scale=1, width=device-width" />
+        </Head>
+
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </CacheProvider>
+    </StyledEngineProvider>
+  );
 }
+
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  emotionCache: PropTypes.object,
+  pageProps: PropTypes.object.isRequired,
+};
+
+export default MyApp;
---
+++ pages/login.js
@@ -0,0 +1,33 @@
+import { Box, Button, Typography } from "@mui/material";
+import { getProviders, signIn } from "next-auth/react";
+
+export async function getServerSideProps() {
+  const providers = await getProviders();
+
+  return {
+    props: {
+      providers,
+    },
+  };
+}
+
+export default function Login(props) {
+  return (
+    <>
+      <Box>
+        <Typography>Pollock Playlist Paneer.</Typography>
+        <Typography>Insert Icon here</Typography>
+        <Typography>Insert snappy tagline here</Typography>
+        {Object.values(props.providers).map((provider) => (
+          <Button
+            variant="contained"
+            key={provider.name}
+            onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+          >
+            Login with {provider.name}
+          </Button>
+        ))}
+      </Box>
+    </>
+  );
+}
